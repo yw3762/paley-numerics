d0433f7a8079790f74e8469b9ba11de7229328e0
Yifan Wang <8groupoid@gmail.com>
2024-02-23 10:46:52 -0500

    Imported MOSEK package and added a sample SDP program

--- helpers.cpp
+++ helpers.cpp
@@ -1,7 +1,7 @@
 #include <algorithm>
 #include <cassert>
 #include <functional>
-#include <iostream>
+#include "fusion.h"
 
 namespace paley::helper {
 
--- main.cpp
+++ main.cpp
@@ -1,9 +1,4 @@
-#include <functional>
-#include <iostream>
-
-#include "helpers.cpp"
-#include "lovasz.cpp"
-#include "block_diagonal.cpp"
+#include "headers.h"
 
 const long long MIN_P = 821;
 const long long MAX_P = 997;
@@ -13,22 +8,17 @@ void compute_and_write(int lower_bound, int upper_bound) {
   auto n_primes = primes.size();
 
   std::vector<double> theta(n_primes), l2(n_primes), l3(n_primes);
+  // TODO: use std::atomic to compute in parallel.
   for (int i = 0; i < n_primes; i++) {
     auto A = paley::helper::get_paley_adjacency(primes[i]);
     theta[i] = paley::relaxation::lovasz_theta(A);
     l2[i] = paley::relaxation::l2(A);
   }
+
+  // TODO: Write the result into .csv files
 }
 
 int main() {
-  std::vector<std::vector<bool>> resultMatrix = paley::helper::get_paley_adjacency(13);
-
-  // Display the result
-  for (const auto& row : resultMatrix) {
-    for (int val : row) {
-      std::cout << val << " ";
-    }
-    std::cout << std::endl;
-  }
+  sdo();
   return 0;
 }
